3a3ef0153c62d00f2defb79ae562ea7dd315f37b
Alessandro Liberatoscioli <alessandro.liberatoscioli.g@gmail.com>
2023-10-21 19:07:07 +0200

    Creation of rest function

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 function sum(a, b) {
   return a + b;
 }
+//Esta funcion toma dos argumentos que representas numeros para utilizar el operador +
+function subtract(a, b) {
+  return a - b;
+}
+
+//Las funciones sum y subtract son funciones en JavaScript que realizan operaciones matemáticas simples.
